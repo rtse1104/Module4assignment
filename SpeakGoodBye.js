f8e96a0f2dc2103c446c1157dbc72044215c9878
rtse1104 <racheljtse@gmail.com>
2020-05-28 15:09:34 -0700

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,10 +1,10 @@
 {function (window) {
-  var speakWord = "Good Bye";
-  var byeSpeaker = {};
-    speak: function (name) {
+ var byeSpeaker = {};
+   var speakWord = "Good Bye";
+    byeSpeaker.speak = function (name) {
        console.log(speakWord + " " + name);
 }
-};
+
 window.byeSpeaker = byeSpeaker;
 
 })(window);
