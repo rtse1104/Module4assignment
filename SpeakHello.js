e15b658abf0edb295e8f82a36d062d048045a24f
rtse1104 <racheljtse@gmail.com>
2020-05-28 15:08:12 -0700

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,9 +1,8 @@
 (function (window) {
-    var speakWord = "Hello";
-    var helloSpeaker = {
-      speak: function (name) {
+   var helloSpeaker = {
+        var speakWord = "Hello";
+      helloSpeaker.speak = function (name) {
         console.log(speakWord + " " + name);
       }
-    };
     window.helloSpeaker = helloSpeaker;
  })(window);
